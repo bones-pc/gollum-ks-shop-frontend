4e738f2b6e11781566b36646273ecdf32d1f6c53
Piotr Cieslak <piotr.cieslak.consultant@nielsen.com>
2022-09-09 15:43:15 +0200

    wip tracking

--- src/order/ManageOrders.svelte
+++ src/order/ManageOrders.svelte
@@ -53,6 +53,7 @@
 		]);
 		orders = o;
 		campaign = c;
+		console.log(o);
 	});
 
 	async function confirm(order: Order & AssignedToUser) {
@@ -81,7 +82,7 @@
 		{/each}
 	</ul>
 
-	<h2>{$_("manage_orders.per_user_summary")}</h2>
+	<h3>{$_("manage_orders.per_user_summary")}</h3>
 	{#each orders as order}
 		{order.firstname}
 		{order.lastname}
@@ -100,6 +101,12 @@
 			<InProgressButton
 				on_click_function={async () => confirm(order)}
 				label="Confirm"
+			/>
+			{$_("manage_orders.tracking")}
+			<input id="tracking" bind:value={order.tracking} />
+			<InProgressButton
+				on_click_function={async () => mark_as_sent(order)}
+				label="Nie wysłane"
 			/>
 		</div>
 		<ul>
