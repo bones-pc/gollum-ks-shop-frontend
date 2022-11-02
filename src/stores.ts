91dc3d02c6864aef92579b9545a3d91297c0f01b
Piotr Cieslak <pcieslak@gmail.com>
2022-11-03 00:43:21 +0100

    gollum.pl

--- src/stores.ts
+++ src/stores.ts
@@ -8,9 +8,9 @@ export const user_uuid: Writable<string> = writable(null);
 export const access_token: Writable<string> = writable(null);
 
 export const api_url: Readable<string> = readable(
-  // "https://www.gollum.pl/api/"
+  "https://www.gollum.pl/api/"
   // "http://localhost:3000/api/"
-  "https://bones.usermd.net/api/"
+  // "https://bones.usermd.net/api/"
 );
 
 export function switchToLoggedUser() {
