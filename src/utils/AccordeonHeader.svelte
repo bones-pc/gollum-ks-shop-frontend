c4d527ff74039e3f2502f1cc5465dd9f4fec1a21
Piotr Cieslak <piotr.cieslak.consultant@nielsen.com>
2022-11-08 02:00:30 +0100

    datey fix

---
+++ src/utils/AccordeonHeader.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+</script>
+
+<div class="container d-none d-md-block">
+	<!-- <div class="img" /> -->
+	<slot name="title" />
+</div>
+
+<style>
+	.img {
+		width: 90px;
+		height: 59px;
+		object-fit: cover;
+		overflow: hidden;
+		float: left;
+		font-size: 1rem;
+		line-height: 26px;
+		text-align: center;
+	}
+
+	.accordion-button {
+		width: none !important;
+		flex: 1;
+	}
+
+	.accordion-button:focus {
+		box-shadow: none !important;
+	}
+</style>
--- src/utils/AccordionList.svelte
+++ src/utils/AccordionList.svelte
@@ -2,6 +2,7 @@
 	import { onMount } from "svelte";
 	import { flip } from "svelte/animate";
 	import { fade } from "svelte/transition";
+	import AccordeonHeader from "./AccordeonHeader.svelte";
 
 	import AccordionListItem from "./AccordionListItem.svelte";
 	import AccordionListNav from "./AccordionListNav.svelte";
@@ -21,6 +22,14 @@
 	<slot slot="actions" name="nav-actions" />
 </AccordionListNav>
 
+<AccordeonHeader>
+	<div class="row mt-3 small" slot="title">
+		<div class="img" />
+		<div class="col-md-5">Nazwa</div>
+		<div class=" col-md-3">Data końca</div>
+		<div class=" col-md-2">Data dodania</div>
+	</div>
+</AccordeonHeader>
 <div class="row campaigns-row">
 	<div class="mb-2 mt-2">
 		<div class="accordion" id="accordionExample">
@@ -54,6 +63,16 @@
 </div>
 
 <style>
+	.img {
+		width: 90px;
+
+		object-fit: cover;
+		overflow: hidden;
+		float: left;
+		font-size: 1rem;
+		line-height: 26px;
+		text-align: center;
+	}
 	.to-right {
 		margin-left: auto;
 		margin-right: 0;
